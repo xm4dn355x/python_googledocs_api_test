bf8be4c398285defcc8540793bd66f1dc1228a39
Michael Nikitenko <xm4dn355x@gmail.com>
2021-08-02 15:26:47 +0500

    gspread is better than google api

--- main.py
+++ main.py
@@ -9,5 +9,21 @@
 ########################################################################
 
 
+from pprint import pprint
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds",
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("instagram").sheet1  # Open the spreadhseet
+
 if __name__ == '__main__':
-    pass
+    sheet.append_row(['4', '5', '6'])
